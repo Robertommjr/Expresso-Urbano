a862262b8ff7ce936a216aa904e022c7ac1ac88d
Roberto_Jr <robertomartins.m.jr@gmail.com>
2016-05-28 21:30:43 -0300

    Base do projeto

---
+++ Projeto/web/ExpressoUrbano/ExpressoUrbano.Presentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpressoUrbano.Presentation.Startup))]
+namespace ExpressoUrbano.Presentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
